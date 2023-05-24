e3d6a66bb7c064a073f35c772dc8d2df2a086dc4
Paige Dudkiewicz <paigedudkiewicz@Paiges-MacBook-Pro.local>
2023-05-24 15:55:01 -0400

    Created product list component, and added mock data

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MenuComponent } from 'src/app/comps/menu/menu.component';
 
 @Component({
   selector: 'ava-website-home-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MenuComponent],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
---
+++ src/mockData/productRes.ts
@@ -0,0 +1,554 @@
+import { productResType } from "./productRes.type";
+
+export const testProductData:productResType[] = [
+    {
+        "id": 16,
+        "name": "Beanie Shirt",
+        "slug": "beanie-shirt",
+        "permalink": "https://18.189.184.33/product/beanie-shirt/",
+        "date_created": "2023-05-03T19:37:49",
+        "date_created_gmt": "2023-05-03T19:37:49",
+        "date_modified": "2023-05-03T19:38:23",
+        "date_modified_gmt": "2023-05-03T19:38:23",
+        "type": "simple",
+        "status": "publish",
+        "featured": false,
+        "catalog_visibility": "visible",
+        "description": "<p>Production description for beanie shirt.</p>\n",
+        "short_description": "<p>Beanie shirt short description will go here.</p>\n",
+        "sku": "",
+        "price": "30",
+        "regular_price": "30",
+        "sale_price": "",
+        "date_on_sale_from": null,
+        "date_on_sale_from_gmt": null,
+        "date_on_sale_to": null,
+        "date_on_sale_to_gmt": null,
+        "on_sale": false,
+        "purchasable": true,
+        "total_sales": 0,
+        "virtual": false,
+        "downloadable": false,
+        "downloads": [],
+        "download_limit": -1,
+        "download_expiry": -1,
+        "external_url": "",
+        "button_text": "",
+        "tax_status": "taxable",
+        "tax_class": "",
+        "manage_stock": false,
+        "stock_quantity": null,
+        "backorders": "no",
+        "backorders_allowed": false,
+        "backordered": false,
+        "low_stock_amount": null,
+        "sold_individually": false,
+        "weight": "",
+        "dimensions": {
+            "length": "",
+            "width": "",
+            "height": ""
+        },
+        "shipping_required": true,
+        "shipping_taxable": true,
+        "shipping_class": "",
+        "shipping_class_id": 0,
+        "reviews_allowed": true,
+        "average_rating": "0.00",
+        "rating_count": 0,
+        "upsell_ids": [],
+        "cross_sell_ids": [],
+        "parent_id": 0,
+        "purchase_note": "",
+        "categories": [
+            {
+                "id": 16,
+                "name": "Test Category",
+                "slug": "test-category"
+            }
+        ],
+        "tags": [
+            {
+                "id": 17,
+                "name": "Tag 1",
+                "slug": "tag-1"
+            },
+            {
+                "id": 18,
+                "name": "Tag 2",
+                "slug": "tag-2"
+            }
+        ],
+        "images": [
+            {
+                "id": 13,
+                "date_created": "2023-04-26T17:57:15",
+                "date_created_gmt": "2023-04-26T17:57:15",
+                "date_modified": "2023-04-26T17:57:15",
+                "date_modified_gmt": "2023-04-26T17:57:15",
+                "src": "https://18.189.184.33/wp-content/uploads/2023/04/DSC_0373-scaled.jpg",
+                "name": "DSC_0373",
+                "alt": ""
+            }
+        ],
+        "attributes": [
+            {
+                "id": 0,
+                "name": "Material",
+                "position": 0,
+                "visible": true,
+                "variation": true,
+                "options": [
+                    "Nylon, cotton, etc."
+                ]
+            },
+            {
+                "id": 0,
+                "name": "Color",
+                "position": 1,
+                "visible": true,
+                "variation": true,
+                "options": [
+                    "Blue, yellow, etc."
+                ]
+            }
+        ],
+        "default_attributes": [],
+        "variations": [],
+        "grouped_products": [],
+        "menu_order": 0,
+        "price_html": "<span class=\"woocommerce-Price-amount amount\"><bdi><span class=\"woocommerce-Price-currencySymbol\">&#36;</span>30.00</bdi></span>",
+        "related_ids": [
+            15,
+            12
+        ],
+        "meta_data": [],
+        "stock_status": "instock",
+        "has_options": false,
+        "_links": {
+            "self": [
+                {
+                    "href": "https://18.189.184.33/wp-json/wc/v3/products/16"
+                }
+            ],
+            "collection": [
+                {
+                    "href": "https://18.189.184.33/wp-json/wc/v3/products"
+                }
+            ]
+        }
+    },
+    {
+        "id": 15,
+        "name": "Beanie Hat",
+        "slug": "beanie-hat",
+        "permalink": "https://18.189.184.33/product/beanie-hat/",
+        "date_created": "2023-05-03T19:36:29",
+        "date_created_gmt": "2023-05-03T19:36:29",
+        "date_modified": "2023-05-03T19:37:34",
+        "date_modified_gmt": "2023-05-03T19:37:34",
+        "type": "simple",
+        "status": "publish",
+        "featured": false,
+        "catalog_visibility": "visible",
+        "description": "<p>Production description for beanie hat.</p>\n",
+        "short_description": "<p>Beanie hat short description will go here.</p>\n",
+        "sku": "",
+        "price": "15",
+        "regular_price": "15",
+        "sale_price": "",
+        "date_on_sale_from": null,
+        "date_on_sale_from_gmt": null,
+        "date_on_sale_to": null,
+        "date_on_sale_to_gmt": null,
+        "on_sale": false,
+        "purchasable": true,
+        "total_sales": 0,
+        "virtual": false,
+        "downloadable": false,
+        "downloads": [],
+        "download_limit": -1,
+        "download_expiry": -1,
+        "external_url": "",
+        "button_text": "",
+        "tax_status": "taxable",
+        "tax_class": "",
+        "manage_stock": false,
+        "stock_quantity": null,
+        "backorders": "no",
+        "backorders_allowed": false,
+        "backordered": false,
+        "low_stock_amount": null,
+        "sold_individually": false,
+        "weight": "",
+        "dimensions": {
+            "length": "",
+            "width": "",
+            "height": ""
+        },
+        "shipping_required": true,
+        "shipping_taxable": true,
+        "shipping_class": "",
+        "shipping_class_id": 0,
+        "reviews_allowed": true,
+        "average_rating": "0.00",
+        "rating_count": 0,
+        "upsell_ids": [],
+        "cross_sell_ids": [],
+        "parent_id": 0,
+        "purchase_note": "",
+        "categories": [
+            {
+                "id": 16,
+                "name": "Test Category",
+                "slug": "test-category"
+            }
+        ],
+        "tags": [
+            {
+                "id": 17,
+                "name": "Tag 1",
+                "slug": "tag-1"
+            },
+            {
+                "id": 18,
+                "name": "Tag 2",
+                "slug": "tag-2"
+            }
+        ],
+        "images": [
+            {
+                "id": 13,
+                "date_created": "2023-04-26T17:57:15",
+                "date_created_gmt": "2023-04-26T17:57:15",
+                "date_modified": "2023-04-26T17:57:15",
+                "date_modified_gmt": "2023-04-26T17:57:15",
+                "src": "https://18.189.184.33/wp-content/uploads/2023/04/DSC_0373-scaled.jpg",
+                "name": "DSC_0373",
+                "alt": ""
+            }
+        ],
+        "attributes": [
+            {
+                "id": 0,
+                "name": "Material",
+                "position": 0,
+                "visible": true,
+                "variation": true,
+                "options": [
+                    "Nylon, cotton, etc."
+                ]
+            },
+            {
+                "id": 0,
+                "name": "Color",
+                "position": 1,
+                "visible": true,
+                "variation": true,
+                "options": [
+                    "Blue, yellow, etc."
+                ]
+            }
+        ],
+        "default_attributes": [],
+        "variations": [],
+        "grouped_products": [],
+        "menu_order": 0,
+        "price_html": "<span class=\"woocommerce-Price-amount amount\"><bdi><span class=\"woocommerce-Price-currencySymbol\">&#36;</span>15.00</bdi></span>",
+        "related_ids": [
+            12,
+            16
+        ],
+        "meta_data": [],
+        "stock_status": "instock",
+        "has_options": false,
+        "_links": {
+            "self": [
+                {
+                    "href": "https://18.189.184.33/wp-json/wc/v3/products/15"
+                }
+            ],
+            "collection": [
+                {
+                    "href": "https://18.189.184.33/wp-json/wc/v3/products"
+                }
+            ]
+        }
+    },
+    {
+        "id": 12,
+        "name": "Beanie Bag",
+        "slug": "beanie-bag",
+        "permalink": "https://18.189.184.33/product/beanie-bag/",
+        "date_created": "2023-04-26T17:39:24",
+        "date_created_gmt": "2023-04-26T17:39:24",
+        "date_modified": "2023-04-26T17:58:36",
+        "date_modified_gmt": "2023-04-26T17:58:36",
+        "type": "simple",
+        "status": "publish",
+        "featured": false,
+        "catalog_visibility": "visible",
+        "description": "<p>Production description for beanie bag.</p>\n",
+        "short_description": "<p>Beanie bag short description will go here.</p>\n",
+        "sku": "",
+        "price": "25",
+        "regular_price": "25",
+        "sale_price": "",
+        "date_on_sale_from": null,
+        "date_on_sale_from_gmt": null,
+        "date_on_sale_to": null,
+        "date_on_sale_to_gmt": null,
+        "on_sale": false,
+        "purchasable": true,
+        "total_sales": 0,
+        "virtual": false,
+        "downloadable": false,
+        "downloads": [],
+        "download_limit": -1,
+        "download_expiry": -1,
+        "external_url": "",
+        "button_text": "",
+        "tax_status": "taxable",
+        "tax_class": "",
+        "manage_stock": false,
+        "stock_quantity": null,
+        "backorders": "no",
+        "backorders_allowed": false,
+        "backordered": false,
+        "low_stock_amount": null,
+        "sold_individually": false,
+        "weight": "",
+        "dimensions": {
+            "length": "",
+            "width": "",
+            "height": ""
+        },
+        "shipping_required": true,
+        "shipping_taxable": true,
+        "shipping_class": "",
+        "shipping_class_id": 0,
+        "reviews_allowed": true,
+        "average_rating": "0.00",
+        "rating_count": 0,
+        "upsell_ids": [],
+        "cross_sell_ids": [],
+        "parent_id": 0,
+        "purchase_note": "",
+        "categories": [
+            {
+                "id": 16,
+                "name": "Test Category",
+                "slug": "test-category"
+            }
+        ],
+        "tags": [
+            {
+                "id": 17,
+                "name": "Tag 1",
+                "slug": "tag-1"
+            },
+            {
+                "id": 18,
+                "name": "Tag 2",
+                "slug": "tag-2"
+            }
+        ],
+        "images": [
+            {
+                "id": 13,
+                "date_created": "2023-04-26T17:57:15",
+                "date_created_gmt": "2023-04-26T17:57:15",
+                "date_modified": "2023-04-26T17:57:15",
+                "date_modified_gmt": "2023-04-26T17:57:15",
+                "src": "https://18.189.184.33/wp-content/uploads/2023/04/DSC_0373-scaled.jpg",
+                "name": "DSC_0373",
+                "alt": ""
+            }
+        ],
+        "attributes": [
+            {
+                "id": 0,
+                "name": "Material",
+                "position": 0,
+                "visible": true,
+                "variation": true,
+                "options": [
+                    "Nylon, cotton, etc."
+                ]
+            },
+            {
+                "id": 0,
+                "name": "Color",
+                "position": 1,
+                "visible": true,
+                "variation": true,
+                "options": [
+                    "Blue, yellow, etc."
+                ]
+            }
+        ],
+        "default_attributes": [],
+        "variations": [],
+        "grouped_products": [],
+        "menu_order": 0,
+        "price_html": "<span class=\"woocommerce-Price-amount amount\"><bdi><span class=\"woocommerce-Price-currencySymbol\">&#36;</span>25.00</bdi></span>",
+        "related_ids": [
+            16,
+            15
+        ],
+        "meta_data": [],
+        "stock_status": "instock",
+        "has_options": false,
+        "_links": {
+            "self": [
+                {
+                    "href": "https://18.189.184.33/wp-json/wc/v3/products/12"
+                }
+            ],
+            "collection": [
+                {
+                    "href": "https://18.189.184.33/wp-json/wc/v3/products"
+                }
+            ]
+        }
+    }
+]
+
+
+const product1:productResType = {
+    "id": 16,
+    "name": "Beanie Shirt",
+    "slug": "beanie-shirt",
+    "permalink": "https://18.189.184.33/product/beanie-shirt/",
+    "date_created": "2023-05-03T19:37:49",
+    "date_created_gmt": "2023-05-03T19:37:49",
+    "date_modified": "2023-05-03T19:38:23",
+    "date_modified_gmt": "2023-05-03T19:38:23",
+    "type": "simple",
+    "status": "publish",
+    "featured": false,
+    "catalog_visibility": "visible",
+    "description": "<p>Production description for beanie shirt.</p>\n",
+    "short_description": "<p>Beanie shirt short description will go here.</p>\n",
+    "sku": "",
+    "price": "30",
+    "regular_price": "30",
+    "sale_price": "",
+    "date_on_sale_from": null,
+    "date_on_sale_from_gmt": null,
+    "date_on_sale_to": null,
+    "date_on_sale_to_gmt": null,
+    "on_sale": false,
+    "purchasable": true,
+    "total_sales": 0,
+    "virtual": false,
+    "downloadable": false,
+    "downloads": [],
+    "download_limit": -1,
+    "download_expiry": -1,
+    "external_url": "",
+    "button_text": "",
+    "tax_status": "taxable",
+    "tax_class": "",
+    "manage_stock": false,
+    "stock_quantity": null,
+    "backorders": "no",
+    "backorders_allowed": false,
+    "backordered": false,
+    "low_stock_amount": null,
+    "sold_individually": false,
+    "weight": "",
+    "dimensions": {
+        "length": "",
+        "width": "",
+        "height": ""
+    },
+    "shipping_required": true,
+    "shipping_taxable": true,
+    "shipping_class": "",
+    "shipping_class_id": 0,
+    "reviews_allowed": true,
+    "average_rating": "0.00",
+    "rating_count": 0,
+    "upsell_ids": [],
+    "cross_sell_ids": [],
+    "parent_id": 0,
+    "purchase_note": "",
+    "categories": [
+        {
+            "id": 16,
+            "name": "Test Category",
+            "slug": "test-category"
+        }
+    ],
+    "tags": [
+        {
+            "id": 17,
+            "name": "Tag 1",
+            "slug": "tag-1"
+        },
+        {
+            "id": 18,
+            "name": "Tag 2",
+            "slug": "tag-2"
+        }
+    ],
+    "images": [
+        {
+            "id": 13,
+            "date_created": "2023-04-26T17:57:15",
+            "date_created_gmt": "2023-04-26T17:57:15",
+            "date_modified": "2023-04-26T17:57:15",
+            "date_modified_gmt": "2023-04-26T17:57:15",
+            "src": "https://18.189.184.33/wp-content/uploads/2023/04/DSC_0373-scaled.jpg",
+            "name": "DSC_0373",
+            "alt": ""
+        }
+    ],
+    "attributes": [
+        {
+            "id": 0,
+            "name": "Material",
+            "position": 0,
+            "visible": true,
+            "variation": true,
+            "options": [
+                "Nylon, cotton, etc."
+            ]
+        },
+        {
+            "id": 0,
+            "name": "Color",
+            "position": 1,
+            "visible": true,
+            "variation": true,
+            "options": [
+                "Blue, yellow, etc."
+            ]
+        }
+    ],
+    "default_attributes": [],
+    "variations": [],
+    "grouped_products": [],
+    "menu_order": 0,
+    "price_html": "<span class=\"woocommerce-Price-amount amount\"><bdi><span class=\"woocommerce-Price-currencySymbol\">&#36;</span>30.00</bdi></span>",
+    "related_ids": [
+        15,
+        12
+    ],
+    "meta_data": [],
+    "stock_status": "instock",
+    "has_options": false,
+    "_links": {
+        "self": [
+            {
+                "href": "https://18.189.184.33/wp-json/wc/v3/products/16"
+            }
+        ],
+        "collection": [
+            {
+                "href": "https://18.189.184.33/wp-json/wc/v3/products"
+            }
+        ]
+    }
+}
