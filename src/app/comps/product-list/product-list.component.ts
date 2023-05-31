7c6fb0c9b606b441b7f0ef92ead071d3948ca02d
Paige Dudkiewicz <paigedudkiewicz@Paiges-MacBook-Pro.local>
2023-05-31 10:16:57 -0400

    generated productList component, added to homepage

--- src/app/comps/product-list/product-list.component.ts
+++ src/app/comps/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { testProductData } from 'src/mockData/productRes';
 
 @Component({
   selector: 'ava-website-product-list',
@@ -8,4 +9,12 @@ import { CommonModule } from '@angular/common';
   templateUrl: './product-list.component.html',
   styleUrls: ['./product-list.component.scss'],
 })
-export class ProductListComponent {}
+export class ProductListComponent {
+  productList: any
+  constructor() {
+    console.log(this.productList)
+    console.log("Product list comp constructor")
+    this.productList= testProductData
+    console.log(this.productList)
+  }
+}
--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MenuComponent } from 'src/app/comps/menu/menu.component';
+import { ProductListComponent } from 'src/app/comps/product-list/product-list.component';
 
 @Component({
   selector: 'ava-website-home-page',
   standalone: true,
-  imports: [CommonModule, MenuComponent],
+  imports: [CommonModule, MenuComponent, ProductListComponent],
   templateUrl: './home-page.component.html',
   styleUrls: ['./home-page.component.scss'],
 })
